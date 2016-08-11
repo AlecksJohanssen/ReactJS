eb8d08b121e353fb65007240f3a81e8471426852
Alecks Johansson <alecksjohansson@Aleckss-MacBook-Pro.local>
2016-08-11 14:25:17 +0700

    Added Login With Facebook

--- index.ios.js
+++ index.ios.js
@@ -11,12 +11,13 @@ import {
   StyleSheet,
   Text,
   Image,
+  PropTypes,
   TouchableOpacity,
   TouchableHighlight,
   View
 } from 'react-native';
 var status = true
-
+var {FBLogin, FBLoginManager} = require('react-native-facebook-login');
 class AwesomeProject2 extends Component {
   constructor(props) {
     super(props)
@@ -57,24 +58,45 @@ class AwesomeProject2 extends Component {
 
   render() {
     let pic = {uri: 'https://scontent-hkg3-1.xx.fbcdn.net/v/t1.0-9/12391348_845858058846132_6507309121831362500_n.jpg?oh=c1385d364a902ba7674605d0a2f5542a&oe=585674FD'};
-
+    var _this = this;
     return (
-      <View style={styles.container}>
-        <Toast visible={this.state.visible}>asdasd</Toast>
-        <Text style={styles.welcome}>
-          Alecks Johanssen
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.jss
-        </Text>
-        <TouchableOpacity onPress={this._onPressButton}>
-          <Image style={styles.imgcorner} source={{uri: this.state.image}} />
-        </TouchableOpacity>
-        <TouchableHighlight onPress={this._onPressButton}>
-          <Text>Button</Text>
-        </TouchableHighlight>
-      </View>
+        <FBLogin style={{ marginBottom: 10, }}
+        ref={(fbLogin) => { this.fbLogin = fbLogin }}
+        permissions={["email","user_friends"]}
+        loginBehavior={FBLoginManager.LoginBehaviors.Browser}
+        onLogin={function(data){
+          console.log("Logged in!");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLogout={function(){
+          console.log("Logged out.");
+          _this.setState({ user : null });
+        }}
+        onLoginFound={function(data){
+          console.log("Existing login found.");
+          console.log(data);
+          _this.setState({ user : data.credentials });
+        }}
+        onLoginNotFound={function(){
+          console.log("No user logged in.");
+          _this.setState({ user : null });
+        }}
+        onError={function(data){
+          console.log("ERROR");
+          console.log(data);
+        }}
+        onCancel={function(){
+          console.log("User cancelled.");
+        }}
+        onPermissionsMissing={function(data){
+          console.log("Check permissions!");
+          console.log(data);
+        }}
+       />
+
     );
+
   }
 }
 
